e9a04843f106cf52b6bde5f1bd74a468adf1e08d
Julian Hernandez <julian1@cimarron.me>
2019-02-17 23:21:14 -0800

    home page working
    On branch html_head

---
+++ library/components/HtmlHead.jsx
@@ -0,0 +1,15 @@
+import Head from 'next/head'
+
+
+function HtmlHead() {
+  return (
+    <Head>
+      <meta charSet="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <meta httpEquiv="X-UA-Compatible" content="ie=edge" />
+      <title>Julian Hernandez - Personal Site</title>
+    </Head>
+  )
+}
+
+export default HtmlHead
---
+++ pages/index.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import HtmlHead from '../library/components/HtmlHead'
+
+
+export default () => {
+  return (
+    <>
+      <HtmlHead />
+      Testing App
+    </>
+  )
+}
