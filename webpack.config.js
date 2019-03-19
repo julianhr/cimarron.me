142c14acde24f58be2f7d7c6b9e7520f1c3c3501
Julian Hernandez <julian1@cimarron.me>
2019-03-18 19:33:25 -0700

    chunk each npm package into its own package

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,5 @@
 const path = require('path')
+const webpack = require('webpack')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
 const CleanWebpackPlugin = require('clean-webpack-plugin')
 
@@ -14,16 +15,13 @@ mdRenderer.link = (href, title, text) => {
 
 
 module.exports = (env, argv) => {
+  const isProd = argv.mode === 'production'
+
   return {
     entry: {
-      index: './src/Root.jsx',
+      main: './src/Root.jsx',
+      // InfiniteScroler: './src/labs/001_infinite_scroller/index.js',
     },
-    output: {
-      path: path.resolve(__dirname, 'dist'),
-      filename: '[name].[hash].bundle.js',
-      publicPath: '/',
-    },
-    devtool: 'inline-source-map',
     module: {
       rules: [
         {
@@ -76,23 +74,59 @@ module.exports = (env, argv) => {
         }
       ]
     },
+    plugins: [
+      new webpack.HashedModuleIdsPlugin(), // so file hashes won't change unexpectedly
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: './src/index.html',
+      }),
+    ],
     resolve: {
       extensions: ['*', '.js', '.jsx'],
       alias: {
         '~': path.resolve(__dirname, 'src')
       }
     },
-    plugins: [
-      new CleanWebpackPlugin(),
-      new HtmlWebpackPlugin({
-        template: './src/index.html',
-      }),
-    ],
+    optimization: {
+      /*
+      many ideas taken from
+      https://hackernoon.com/the-100-correct-way-to-split-your-chunks-with-webpack-f8a9df5b7758
+      */
+      runtimeChunk: 'single',
+      splitChunks: {
+        chunks: 'all',
+        maxInitialRequests: Infinity,
+        minSize: 0,
+        cacheGroups: {
+          default: false,
+          vendors: false,
+          vendor: {
+            test: /node_modules/,
+            name(module) {
+              // get package name and group file under it
+              const pnpmSignature = '.registry.npmjs.org'
+              const pnpmRegEx = /\.registry\.npmjs\.org\/([^/]+)\//
+              const npmRegEx = /node_modules\/([^/]+)\//
+              const npmPackageNameRegex =
+                module.context.includes(pnpmSignature) ? pnpmRegEx : npmRegEx
+              const packageName = module.context.match(npmPackageNameRegex)[1]
+              // npm package names are URL-safe, but some servers don't like @ symbols
+              return `npm.${packageName.replace('@', '')}`
+            },
+          },
+        },
+      },
+    },
+    output: {
+      path: path.resolve(__dirname, 'dist'),
+      filename: isProd ? '[name].[contenthash].js' : '[name].js',
+      publicPath: '/',
+    },
+    devtool: 'inline-source-map',
     devServer: {
       historyApiFallback: true,
       hot: true,
       port: 3000,
-      stats: 'normal',
       // open: true,
     },
   }
