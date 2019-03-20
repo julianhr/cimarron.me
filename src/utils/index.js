9fba093cf5c48a87586d877ccbba8f620479128f
Julian Hernandez <julian1@cimarron.me>
2019-03-20 03:52:15 -0700

    update backend endpoints to https

--- src/utils/index.js
+++ src/utils/index.js
@@ -5,7 +5,7 @@ export function urlBuilder(basePath, query={}, basePort, api='api.flask') {
 
   basePath = basePath.trim()
   const isProduction = process.env.NODE_ENV === 'production'
-  const host = isProduction ? `http://${api}.cimarron.me` : 'http://localhost'
+  const host = isProduction ? `https://${api}.cimarron.me` : 'http://localhost'
   const port = basePort === undefined ? (isProduction ? '80' : '5000') : basePort
   const path = basePath.length > 0 && basePath[0] === '/' ? basePath.slice(1) : basePath
   const urlBase = `${host}:${port}/${path}`
