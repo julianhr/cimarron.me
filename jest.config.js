6ee99e2fca58144a8e53d9ea18bf034034a68289
Julian Hernandez <julian1@cimarron.me>
2019-03-27 02:08:48 -0700

    configure Jest to handle React and Webpack correctly

--- jest.config.js
+++ jest.config.js
@@ -77,6 +77,13 @@ module.exports = {
 
   // A map from regular expressions to module names that allow to stub out resources with a single module
   // moduleNameMapper: {},
+  moduleNameMapper: {
+    "\\.(jpg|jpeg|png|gif|eot|otf|webp|svg|ttf|woff|woff2|mp4|webm|wav|mp3|m4a|aac|oga)$": "<rootDir>/jest/__mocks__/fileMock.js",
+    "\\.(css|less)$": "<rootDir>/jest/__mocks__/styleMock.js",
+    "^~(.*)$": "<rootDir>/src$1",
+    // "^react(.*)$": "<rootDir>/vendor/react-master$1",
+    // "^config$": "<rootDir>/configs/app-config.js",
+  },
 
   // An array of regexp pattern strings, matched against all module paths before considered 'visible' to the module loader
   // modulePathIgnorePatterns: [],
@@ -103,7 +110,8 @@ module.exports = {
   // resetModules: false,
 
   // A path to a custom resolver
-  // resolver: null,
+  // resolver: null
+  resolver: require.resolve(`jest-pnp-resolver`),
 
   // Automatically restore mock state between every test
   // restoreMocks: false,
@@ -127,8 +135,10 @@ module.exports = {
 
   // A list of paths to snapshot serializer modules Jest should use for snapshot testing
   // snapshotSerializers: [],
+  snapshotSerializers: ['jest-emotion'],
 
   // The test environment that will be used for testing
+  // testEnvironment: "jsdom",
   testEnvironment: "jest-environment-jsdom-fourteen",
 
   // Options that will be passed to the testEnvironment
@@ -164,7 +174,7 @@ module.exports = {
   // timers: "real",
 
   // A map from regular expressions to paths to transformers
-  // transform: null,
+  // transform: null
 
   // An array of regexp pattern strings that are matched against all source file paths, matched files will skip transformation
   // transformIgnorePatterns: [
