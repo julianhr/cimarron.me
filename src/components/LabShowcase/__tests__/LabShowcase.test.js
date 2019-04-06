2b447266df6868ded4bdba492b02275b4137590a
Julian Hernandez <julian1@cimarron.me>
2019-04-06 14:11:29 -0700

    update LabShowcase tests

--- src/components/LabShowcase/__tests__/LabShowcase.test.js
+++ src/components/LabShowcase/__tests__/LabShowcase.test.js
@@ -4,21 +4,16 @@ import { render } from 'enzyme'
 import LabShowcase from '../LabShowcase'
 import withAppRoot from '~/components/library/withAppRoot'
 import labsData from '~/__tests__/__fixtures__/labsDataFixture'
-import throwPropErrorsMock from '~/__tests__/__mocks__/throwPropErrorsMock'
 
 
 describe('LabShowcase', () => {
-  it('should match snaspshot', () => {
+  it('should match snapshot', () => {
     const RootedLabShowcase = withAppRoot(LabShowcase, { labs: labsData })
     const wrapper = render(<RootedLabShowcase />)
     expect(wrapper).toMatchSnapshot()
   })
 
   describe('required props', () => {
-    beforeAll(() => {
-      throwPropErrorsMock()
-    })
-
     it('labs', () => {
       const RootedLabShowcase = withAppRoot(LabShowcase)
       expect(() => render(<RootedLabShowcase />)).toThrow()
