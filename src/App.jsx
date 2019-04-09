0821db4064d996e7cf71c1e6a13af581e2815134
Julian Hernandez <julian1@cimarron.me>
2019-04-09 14:16:53 -0700

    update App

--- src/App.jsx
+++ src/App.jsx
@@ -1,14 +1,37 @@
 import React from 'react'
+import styled from '@emotion/styled'
 
 import LabShowcase from './components/LabShowcase/LabShowcase'
-import withAppRoot from './components/library/withAppRoot'
-import withAppShell from './components/library/withAppShell'
-import labs from './labs/labsData'
+import TopNav from '~/components/TopNav/TopNav'
 import appStore from './reducers'
+import withAppRoot from './components/library/withAppRoot'
+import labs from './labs/labsData'
 
+
+const Container = styled.div`
+  display: grid;
+  grid:
+    "left-blank main right-blank" 100vh
+    / auto minmax(auto, 1000px) auto;
+`
+
+const Main = styled.main`
+  grid-area: main;
+`
+
+const MENU_ITEMS = [
+  { url: '/', label: 'Home' },
+]
 
 function App() {
-  return <LabShowcase labs={labs} />
+  return (
+    <Container>
+      <Main>
+        <TopNav menuItems={MENU_ITEMS} />
+        <LabShowcase labs={labs} />
+      </Main>
+    </Container>
+  )
 }
 
-export default withAppRoot(withAppShell(App), appStore)
+export default withAppRoot(App, appStore)
