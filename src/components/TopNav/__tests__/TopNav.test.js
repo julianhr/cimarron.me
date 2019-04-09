0b62b021170f0ac42d77dc64ed822579fc8b1433
Julian Hernandez <julian1@cimarron.me>
2019-04-09 14:15:11 -0700

    force menu items to be passed as props in TopNav

--- src/components/TopNav/__tests__/TopNav.test.js
+++ src/components/TopNav/__tests__/TopNav.test.js
@@ -1,13 +1,30 @@
 import React from 'react'
 import { mount } from 'enzyme'
+import { create } from 'react-test-renderer'
 
 import TopNav from '../TopNav'
 import MockApp from '~/__tests__/__mocks__/MockApp'
 
 
+const testProps = {
+  menuItems: [
+    { url: '/one/path', label: 'label 1', isLinkRouted: false },
+  ]
+}
+
+const renderApp = (props) => create(<MockApp><TopNav {...props} /></MockApp>)
+
 describe('TopNav', () => {
+  describe('props', () => {
+    it('requires menuItems', () => {
+      const props = {...testProps}
+      delete props.menuItems
+      expect(() => renderApp(props)).toThrow()
+    })
+  })
+
   it('should match snapshot', () => {
-    const wrapper = mount(<MockApp><TopNav /></MockApp>)
+    const wrapper = mount(<MockApp><TopNav {...testProps} /></MockApp>)
     expect(wrapper).toMatchSnapshot()
     wrapper.unmount()
   })
