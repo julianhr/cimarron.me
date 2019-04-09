f4b8bada1cc65971254e18297f5c723c6b4cf811
Julian Hernandez <julian1@cimarron.me>
2019-04-09 02:30:49 -0700

    various InfiniteScroller tests updated

--- src/labs/001_InfiniteScroller/__tests__/InfiniteScroller.test.js
+++ src/labs/001_InfiniteScroller/__tests__/InfiniteScroller.test.js
@@ -1,15 +1,20 @@
 import React from 'react'
 import { mount } from 'enzyme'
 
-import InfiniteScroller from '../InfiniteScroller'
-import withAppRoot from '~/components/library/withAppRoot'
+import { InfiniteScroller } from '../InfiniteScroller'
+import MockInfiniteScrollerApp from '../__tests__/__mocks__/MockInfiniteScrollerApp'
 
 
 describe('index.js', () => {
   it('mounts without crashing', () => {
-    const RootedIndex = withAppRoot(InfiniteScroller)
     let wrapper
-    expect(() => { wrapper = mount(<RootedIndex />) }).not.toThrow()
+
+    expect(() => wrapper = mount(
+      <MockInfiniteScrollerApp>
+        <InfiniteScroller />
+      </MockInfiniteScrollerApp>
+    )).not.toThrow()
+
     wrapper.unmount()
   })
 })
