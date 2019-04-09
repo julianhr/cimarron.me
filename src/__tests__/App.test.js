0d4bfd8752a5b545009a8bc5bf0a70dcb556b9c0
Julian Hernandez <julian1@cimarron.me>
2019-04-09 01:12:20 -0700

    App updated to allow multiple entry points

--- src/__tests__/App.test.js
+++ src/__tests__/App.test.js
@@ -1,12 +1,11 @@
 import React from 'react'
-import ReactDOM from 'react-dom'
+import { mount } from 'enzyme'
+
 import App from '../App'
 
 
 describe('App', () => {
   it('renders without crashing', () => {
-    const div = document.createElement('div')
-    ReactDOM.render(<App />, div)
-    ReactDOM.unmountComponentAtNode(div)
+    expect(() => mount(<App />)).not.toThrow()
   })
 })
