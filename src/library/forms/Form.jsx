a8837b59a2921572d1c08152e1e532a039d18aeb
Julian Hernandez <julian1@cimarron.me>
2019-04-16 03:21:17 -0700

    Form component

---
+++ src/library/forms/Form.jsx
@@ -0,0 +1,86 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+import styled from '@emotion/styled'
+
+
+const FormTag = styled.form`
+  display: flex;
+  flex-direction: column;
+
+  div.field {
+    margin-bottom: 15px;
+  }
+
+  label {
+    padding: 0 10px 4px;
+    width: 100%;
+    display: flex;
+    color: ${props => props.theme.colors.primary.dark};
+    font-size: 1.1em;
+  }
+
+  input, textarea {
+    width: 100%;
+    margin-bottom: 5px;
+
+    :focus {
+      outline: none;
+    }
+  }
+
+  input {
+    padding: 10px 12px;
+  }
+
+  textarea {
+    padding: 12px;
+  }
+
+  span {
+    color: ${props => props.theme.colors.error};
+    font-size: 18px;
+    padding: 0 10px;
+  }
+
+  button[type=submit] {
+    margin-top: 20px;
+  }
+`
+
+function Form({ children, formStyle, onSubmit }) {
+  return (
+    <FormTag
+      css={formStyle}
+      method={'POST'}
+      onSubmit={onSubmit}
+      onKeyPress={handleFormOnKeyPress}
+    >
+      {children}
+    </FormTag>
+  )
+}
+
+export function handleFormOnKeyPress(event) {
+  if (event.key !== 'Enter') { return null }
+
+  // allow user to press Enter inside a textarea tag
+  // insert a line break instead of submitting the form
+  if (event.target.type === 'textarea') {
+    event.stopPropagation()
+  }
+}
+
+Form.propTypes = {
+  formStyle: PropTypes.object,
+  onSubmit: PropTypes.func.isRequired,
+  children: PropTypes.oneOfType([
+    PropTypes.element,
+    PropTypes.arrayOf(PropTypes.element)
+  ]).isRequired,
+}
+
+Form.defaultProps = {
+  formStyle: {},
+}
+
+export default Form
