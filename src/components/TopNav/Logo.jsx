7c826dc483b93b50cf23a5c72f23b5543e33be58
Julian Hernandez <julian1@cimarron.me>
2019-04-28 19:15:20 -0700

    update TopNav to allow routed/unrouted links to logo

--- src/components/TopNav/Logo.jsx
+++ src/components/TopNav/Logo.jsx
@@ -3,6 +3,7 @@ import PropTypes from 'prop-types'
 import styled from '@emotion/styled'
 
 import config from '~/config'
+import AnchorTag from '~/library/AnchorTag'
 
 
 const Root = styled.div`
@@ -18,20 +19,31 @@ const H3 = styled.h3`
 `
 
 const H6 = styled.h6`
-  padding-top: 4px;
+  padding-top: 2px;
   font-size: 11px;
   font-weight: 300;
   margin: 0;
   color: #999;
 `
 
-function LeftNav() {
+function Logo({ isLinkRouted }) {
   return (
     <Root>
-      <H3>{config.author.name}</H3>
-      <H6>{config.author.title}</H6>
+      <AnchorTag
+        url='/'
+        label='Home'
+        rootStyle={{ textDecoration: 'none' }}
+        isLinkRouted={isLinkRouted}
+      >
+        <H3>{config.author.name}</H3>
+        <H6>{config.author.title}</H6>
+      </AnchorTag>
     </Root>
   )
 }
 
-export default LeftNav
+Logo.propTypes = {
+  isLinkRouted: PropTypes.bool,
+}
+
+export default Logo
